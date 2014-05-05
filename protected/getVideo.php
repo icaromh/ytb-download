501ce18a406abee9424cb60f0707b38e6991415f
icaromh2 <icaro@icaromh.com>
2014-05-04 21:21:34 -0300

    Adicionada validação para links curtos do youtube

--- protected/getVideo.php
+++ protected/getVideo.php
@@ -1,7 +1,15 @@
 <?php 
 if(isset($_POST['address']) && !empty($_POST['address'])):
+    
     $url = explode('#t',$_POST['address']);
     $url = reset($url);
+
+    if( strpos($url,'youtu.be') !== false){
+        $url = explode('/', $url);
+        $url = end($url);
+        $url = 'https://www.youtube.com/watch?v=' . $url;
+    }
+
     $res = exec('./youtubeDown.sh ' . $url);
     $res = urlencode($res);
     echo json_encode(array('success' => true, 'url' => $res));
