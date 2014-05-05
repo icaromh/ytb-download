72f915a4fd38ef156e7f1659a97af86db9bbb931
icaromh2 <icaro@icaromh.com>
2014-05-04 21:25:00 -0300

    Adicionado Analitcs

--- index.php
+++ index.php
@@ -44,6 +44,15 @@
 
         </div>
 
+        <script>
+              (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+              (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+              })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+              ga('create', 'UA-50597564-1', 'icaromh.com');
+              ga('send', 'pageview');
+        </script>
         <!-- Scripts  -->
         <script type="text/javascript" src="assets/js/jquery-1.11.1.min.js"></script>
         <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
