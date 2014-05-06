1b6235cafaab42988554203680d610db3f86d37d
icaromh2 <icaro@icaromh.com>
2014-05-05 21:52:18 -0300

    Ajustado para formato mp3 e ajustado download firefox

--- get.php
+++ get.php
@@ -1,9 +1,9 @@
 <?php
 if(isset($_GET['m']) && !empty($_GET['m'])){
     $file = urldecode($_GET['m']);
-    header("Content-type: octet/stream");
-    header("Content-disposition: attachment; filename=".$file.";");
-    header("Content-Length: ".filesize('musics/'.$file));
+    header("Content-type: audio/mpeg");
+    header("Content-disposition: attachment; filename=" . $file);
+    // header("Content-Length: " . filesize('musics/'.$file));
     readfile('musics/'.$file);
     exit;
 }
