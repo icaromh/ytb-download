23efdf5ffb9f2c0a80595bb78923600e139dda8d
root <root@icaromh.(none)>
2014-05-06 16:11:10 +0000

    Correção filename firefox

--- get.php
+++ get.php
@@ -1,9 +1,15 @@
 <?php
 if(isset($_GET['m']) && !empty($_GET['m'])){
-    $file = basename(urldecode($_GET['m']));
-    header("Content-type: audio/mpeg");
-    header("Content-disposition: attachment; filename=" . $file . '.mp3');
-    // header("Content-Length: " . filesize('musics/'.$file));
-    readfile('musics/'.$file);
-    exit;
+	$file = urldecode($_GET['m']);
+
+	header('Content-Description: File Transfer');
+	header('Content-Disposition: attachment; filename="'.$file.'"');
+	header('Content-Type: application/octet-stream');
+	header('Content-Transfer-Encoding: binary');
+	header('Content-Length: ' . filesize('musics/'.$file));
+	header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+	header('Pragma: public');
+	header('Expires: 0');
+	readfile('musics/'.$file);
+	exit;
 }
