bbc0a7839b41dc3769a9f41c9b45d4f91ebca5e5
icaromh2 <icaro@icaromh.com>
2014-05-06 15:54:23 +0000

    Testes para o firefox

--- get.php
+++ get.php
@@ -1,8 +1,8 @@
 <?php
 if(isset($_GET['m']) && !empty($_GET['m'])){
-    $file = urldecode($_GET['m']);
+    $file = basename(urldecode($_GET['m']));
     header("Content-type: audio/mpeg");
-    header("Content-disposition: attachment; filename=" . $file);
+    header("Content-disposition: attachment; filename=" . $file . '.mp3');
     // header("Content-Length: " . filesize('musics/'.$file));
     readfile('musics/'.$file);
     exit;
